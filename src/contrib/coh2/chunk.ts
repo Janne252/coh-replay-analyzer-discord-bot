4e0b747298978688af134fc973a9caf268ceebf3
Janne <janne.a.varjo@gmail.com>
2020-10-02 19:53:56 +0300

    code style to match EcmaScript

--- src/contrib/coh2/chunk.ts
+++ src/contrib/coh2/chunk.ts
@@ -13,64 +13,62 @@ export enum ChunkParseMode {
 // Implementation based on Corsix' https://github.com/corsix/coh2-formats and
 // Copernicus' CoH2_RGDTools (source unavailable as of 2020/10/2)
 export class Chunk {
-    public Version: number;
-    public Type: ChunkType;
-    public Kind: string;
-    public DataSize: number;
-    public NameSize: number;
-    public Unknown01: number;
-    public Unknown02: number;
-    public Name: string;
-    public Data: Buffer;
-    public readonly MinVersion: number;
-    public readonly Flags: number;
+    public readonly version: number;
+    public readonly type: ChunkType;
+    public readonly kind: string;
+    public readonly dataLength: number;
+    public readonly nameLength: number;
+    public readonly name: string;
+    public readonly minVersion: number;
+    public readonly flags: number;
+    public data: Buffer;
     public children: Chunk[] = [];
 
-    public readonly IsValid: boolean;
+    public readonly isValid: boolean;
     constructor(public readonly Chunky: RelicChunky, reader: BinaryReader, mode = ChunkParseMode.Strict) {
-        this.Type = reader.readString(4, 'ascii') as ChunkType;
-        this.Kind = reader.readString(4, 'ascii');
-        this.IsValid = this.Type === ChunkType.Data || this.Type === ChunkType.Folder;;
-        if (!this.IsValid) {
+        this.type = reader.readString(4, 'ascii') as ChunkType;
+        this.kind = reader.readString(4, 'ascii');
+        this.isValid = this.type === ChunkType.Data || this.type === ChunkType.Folder;;
+        if (!this.isValid) {
              if (mode === ChunkParseMode.Graceful) {
                 // Roll back reading UInt32 x 2
                 reader.position -= 8;
                 return;
             }
-            throw new Error(`Invalid chunk type "${this.Type}"`); 
+            throw new Error(`Invalid chunk type "${this.type}"`); 
         }
 
-        this.Version = reader.ReadUInt32();
-        this.DataSize = reader.ReadUInt32();
-        this.NameSize = reader.ReadUInt32();
-        if (this.Chunky.Version >= 2) {
-            this.MinVersion = reader.ReadUInt32();
+        this.version = reader.ReadUInt32();
+        this.dataLength = reader.ReadUInt32();
+        this.nameLength = reader.ReadUInt32();
+        if (this.Chunky.version >= 2) {
+            this.minVersion = reader.ReadUInt32();
         }
-        if (this.Chunky.Version >= 3) {
-            this.Flags = reader.ReadUInt32();
+        if (this.Chunky.version >= 3) {
+            this.flags = reader.ReadUInt32();
         }
 
-        this.Name = reader.readString(this.NameSize, 'ascii');
+        this.name = reader.readString(this.nameLength, 'ascii');
         
-        if (this.Type === ChunkType.Folder) {
-            this.children.push(...Chunk.readChunks(this.Chunky, reader, {endOffset: reader.position + this.DataSize, mode}));
+        if (this.type === ChunkType.Folder) {
+            this.children.push(...Chunk.readChunks(this.Chunky, reader, {endOffset: reader.position + this.dataLength, mode}));
         } else {
-            this.ReadData(reader);
+            this.readData(reader);
         }
     }
 
-    protected ReadData(reader: BinaryReader) {
-        this.Data = reader.read(this.DataSize);
+    protected readData(reader: BinaryReader) {
+        this.data = reader.read(this.dataLength);
     }
 
     public static readChunks(chunky: RelicChunky, reader: BinaryReader, {endOffset, mode}: {endOffset?: number, mode: ChunkParseMode}) {
         const result: Chunk[] = [];
         while (true) {
             const chunk = new Chunk(chunky, reader, mode);
-            if (chunk.IsValid) {
+            if (chunk.isValid) {
                 result.push(chunk);
             }
-            if (reader.position == (endOffset ?? reader.length) || (mode === ChunkParseMode.Graceful && !chunk.IsValid)) {
+            if (reader.position == (endOffset ?? reader.length) || (mode === ChunkParseMode.Graceful && !chunk.isValid)) {
                 break;
             }
         }
@@ -79,27 +77,29 @@ export class Chunk {
 }
 
 export class RelicChunky {
-    public Name: string;
-    public Magic: string;
-    public Signature: number;
-    public Version: number;
-    public Platform: number;
-    public FileHeaderSize: number = 36;
-    public ChunkHeaderSize: number;
-    public MinVersion: number;
-    public Chunks: Chunk[] = [];
+    public readonly name: string;
+    public readonly magic: string;
+    // In Corsix' implementation "magi"c is "Relic Chunky" and the signature is processed separately as below
+    // For comparison Copernicus reads the signature as part of the "magic": "Relic Chunky\r\n\u001a\0"
+    public readonly signature: number;
+    public readonly version: number;
+    public readonly platform: number;
+    public readonly fileHeaderSize: number = 36;
+    public readonly chunkHeaderSize: number;
+    public readonly minVersion: number;
+    public readonly chunks: Chunk[] = [];
 
     constructor(reader: BinaryReader, mode = ChunkParseMode.Strict) {
-        this.Magic = reader.readString(12, 'ascii');
-        this.Signature = reader.ReadUInt32();
-        this.Version = reader.ReadUInt32();
-        this.Platform = reader.ReadUInt32();
-        if (this.Version >= 3) {
-            this.FileHeaderSize = reader.ReadUInt32();
-            this.ChunkHeaderSize = reader.ReadUInt32();
-            this.MinVersion = reader.ReadUInt32();
+        this.magic = reader.readString(12, 'ascii');
+        this.signature = reader.ReadUInt32();
+        this.version = reader.ReadUInt32();
+        this.platform = reader.ReadUInt32();
+        if (this.version >= 3) {
+            this.fileHeaderSize = reader.ReadUInt32();
+            this.chunkHeaderSize = reader.ReadUInt32();
+            this.minVersion = reader.ReadUInt32();
         }
 
-        this.Chunks.push(...Chunk.readChunks(this, reader, {mode}));
+        this.chunks.push(...Chunk.readChunks(this, reader, {mode}));
     }
 }
