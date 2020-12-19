cecf7d0b7fd45e877c45787d11313689c8777dcc
Janne <janne.a.varjo@gmail.com>
2020-12-19 20:09:59 +0200

    locale tests

--- src/contrib/coh2/index.spec.ts
+++ src/contrib/coh2/index.spec.ts
@@ -1,7 +1,30 @@
 import assert from 'assert';
 import {describe, it} from 'mocha';
+import { Locale } from '.';
+import i18n from '../i18n';
 
 describe('contrib.coh2.index', () => {
-  
+    it('Locale', () => {
+        const locale = new Locale({'en': {1: 'foo', 1234: 'bar'}, 'fr': {1: 'toto', 1234: 'rade'}});
+
+        const restoreLocale = i18n.activate('en');
+        // Uses i18n.activeLocale
+        assert.strictEqual(locale.get(1), 'foo');
+        assert.strictEqual(locale.get(1234), 'bar');
+
+        // User-defined locale
+        assert.strictEqual(locale.get(1, 'en'), 'foo');
+        assert.strictEqual(locale.get(1234, 'en'), 'bar');
+        assert.strictEqual(locale.get(2, 'en'), undefined);
+
+        assert.strictEqual(locale.get(1, 'fr'), 'toto');
+        assert.strictEqual(locale.get(1234, 'fr'), 'rade');
+        assert.strictEqual(locale.get(2, 'fr'), undefined);
+
+        // non-existent locale
+        assert.strictEqual(locale.get(1, 'not-existent locale'), undefined);
+
+        restoreLocale();
+    });
 });
 
