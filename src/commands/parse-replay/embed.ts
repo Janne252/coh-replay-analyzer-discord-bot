f19d2d46f23e494bf768179a46a26630e762c672
Janne <janne.a.varjo@gmail.com>
2021-03-13 23:02:14 +0200

    link to support server

--- src/commands/parse-replay/embed.ts
+++ src/commands/parse-replay/embed.ts
@@ -7,7 +7,7 @@ import path from 'path';
 import { InputData } from '../../types';
 import { ReplayPlayer } from '../../contrib/coh2/replay';
 import { autoDeleteRelatedMessages, truncatedEmbedCodeField } from '../../contrib/discord';
-import { coh2Locale, logger } from '../..';
+import { coh2Locale, diagnosticsConfig, logger } from '../..';
 import { Char } from '../../contrib/misc';
 import { InputMessage } from '.';
 import { LogLevel } from '../../contrib/discord/logging';
@@ -132,7 +132,7 @@ export abstract class ReplayBaseEmbed extends Discord.MessageEmbed {
     }
 
     protected appendNoScenarioPreviewImageAvailable() {
-        this.addField('\u200b', `_${i18n.get('replay.noMapPreviewImageAvailable')}_`);
+        this.addField('\u200b', `_${i18n.get('replay.noMapPreviewImageAvailable')}_\n${`[${i18n.get('replay.requestCustomMapSupportLinkTitle')}](${diagnosticsConfig.support.inviteUrl})`}`);
     }
 
     protected appendMetadata({duration, gameVersion}: {duration?: boolean, gameVersion?: boolean} = {}) {
