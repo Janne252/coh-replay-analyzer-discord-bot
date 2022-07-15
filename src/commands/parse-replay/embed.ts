4743661ac6285b826bf986fae5e22b6fa27154d9
Matei Marica <matei.marica@outlook.com>
2022-07-15 01:38:05 -0300

    changed ladder url to coh2.org

--- src/commands/parse-replay/embed.ts
+++ src/commands/parse-replay/embed.ts
@@ -275,7 +275,7 @@ export abstract class ReplayBaseEmbed extends Discord.MessageEmbed {
         if (player.steam_id_str && player.steam_id_str != '0') {
             const url = (
                 this.config.leaderboardUrl ?? 
-                `http://www.companyofheroes.com/leaderboards#profile/steam/{steamId}/standings`
+                `https://www.coh2.org/ladders/playercard/steamid/{steamId}`
             )
                 .replace(/\{steamId\}/g, player.steam_id_str)
             ;
